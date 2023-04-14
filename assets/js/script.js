c19ae004479f38c0e26cb14c42a1727d74ab3492
Developer-Zahid <coderexpertzahid@gmail.com>
2023-04-15 02:08:49 +0600

    change system

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,14 @@ progressbarElement.style.strokeDasharray = progressbarTotalLength;
 progressbarElement.style.strokeDashoffset = progressbarTotalLength;
 
 // Get the countup time inputs from the Dom
-const countupHours = parseInt(counterElement.dataset.startHours);
-const countupMinutes = parseInt(counterElement.dataset.startMinutes);
-const countupSeconds = parseInt(counterElement.dataset.startSeconds);
+const countupHours = parseInt((24 - 1) - counterElement.dataset.endHours);
+const countupMinutes = parseInt((60 - 1) - counterElement.dataset.endMinutes);
+const countupSeconds = parseInt((60 - 1) - counterElement.dataset.endSeconds);
 
-// Get the countup time inputs from the Dom
-const countdownHours = parseInt((24 - 1) - counterElement.dataset.startHours);
-const countdownMinutes = parseInt((60 - 1) - counterElement.dataset.startMinutes);
-const countdownSeconds = parseInt((60 - 1) - counterElement.dataset.startSeconds);
+// Get the countdown time inputs from the Dom
+const countdownHours = parseInt(counterElement.dataset.endHours);
+const countdownMinutes = parseInt(counterElement.dataset.endMinutes);
+const countdownSeconds = parseInt(counterElement.dataset.endSeconds);
 
 // Convert the inputs to milliseconds and set the start date & target date
 const countupTime = (countupHours * 60 * 60 + countupMinutes * 60 + countupSeconds) * 1000;
@@ -25,35 +25,15 @@ const countdownTime = (countdownHours * 60 * 60 + countdownMinutes * 60 + countd
 const startDate = new Date().getTime() - countupTime;
 const targetDate = new Date().getTime() + countdownTime;
 
-// Update the countup every second
-const countup = setInterval(() => {
-  // Get the current date and time
-  const now = new Date().getTime();
 
-  // Calculate the elapsed time
-  const elapsedTime = now - startDate;
-
-  
-  // Calculate seconds
-  const countupSeconds = Math.floor(elapsedTime / 1000);
-
-  progressbarElement.style.strokeDashoffset = progressbarTotalLength - (progressbarTotalLength * countupSeconds) / (24 * 60 * 60);
-
-  // Stop the countup after 24 hours
-  if (elapsedTime >= 24 * 60 * 60 * 1000) {
-    clearInterval(countup);
-    console.log("24 hour countup complete!");
-  }
-}, 1000); // Update every second
-
-
-// Update the countup every second
+// Update the countdown every second
 const countdown = setInterval(() => {
   // Get the current date and time
   const now = new Date().getTime();
 
   // Calculate the remaining time
   const remainingTime = targetDate - now;
+  const elapsedTime = now - startDate;
 
   // Calculate countdown hours, minutes, and seconds
   const countdownHours = Math.floor(remainingTime / (60 * 60 * 1000));
@@ -64,11 +44,15 @@ const countdown = setInterval(() => {
   minutesElement.innerText = countdownMinutes < 10 ? `0${countdownMinutes}` : countdownMinutes;
   secondsElement.innerText = countdownSeconds < 10 ? `0${countdownSeconds}` : countdownSeconds;
 
+   // Calculate seconds
+   const countupSeconds = Math.floor(elapsedTime / 1000);
+
+   progressbarElement.style.strokeDashoffset = progressbarTotalLength - (progressbarTotalLength * countupSeconds) / (24 * 60 * 60);
+
     // Stop the countdown when the target date is reached
     if (remainingTime <= 0) {
         clearInterval(countdown);
         counterElement.innerText = counterElement.dataset.timerEndMessage;
-        console.log("24 hour countdown complete!");
     }
 }, 1000); // Update every second
 
